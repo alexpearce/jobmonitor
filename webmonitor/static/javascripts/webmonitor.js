2354a82c910cd2bf3e189a5a31dd8e0be622c0b6
Alex Pearce <alex@alexpearce.me>
2014-07-04 15:00:44 +0100

    Update the WebMonitor JS object for the new API.
    This is also a substantial refactor, and should allow derivative apps to interface with the monitor much more concisely.

--- webmonitor/static/javascripts/webmonitor.js
+++ webmonitor/static/javascripts/webmonitor.js
@@ -58,7 +58,6 @@ var WebMonitor = (function($, undefined) {
   //   undefined
   var drawHistogram = function(container, data, options) {
     var opt = $.extend(true, {}, WebMonitor.settings.histogramDefaults, options);
-    console.log(container.width(), container.height());
     d3.select(container.get()[0]).append('svg')
       .attr('width', container.width())
       .attr('height', container.height())
@@ -115,81 +114,136 @@ var WebMonitor = (function($, undefined) {
     drawHistogram(container, formattedData, options);
   };
 
-  // Display msg inside container, styled as a red error box
-  // Accepts:
-  //   container: jQuery element to insert msg into
-  //   msg: HTML message to display inside container
-  var displayFailure = function(container, msg) {
-    container.html('<div class="alert alert-danger">' + msg + '</div>');
-  };
-
   // Fetches and draws the named `histogram`, residing in `file`, in to the `container`.
   // Accepts:
   //   histogram: String of the histogram's full path key name with in the file
   //   file: String of the file name
   //   container: jQuery element the histogram should be drawn in to. Any existing content will be replaced.
   var loadHistogramFromFileIntoContainer = function(histogram, file, container) {
-    var url = '/files/' + file + '/' + histogram;
-    var failMsg = 'There was a problem retrieving histogram <code>'
-      + histogram
-      + '</code> from file <code>'
-      + file
-      + '</code>. Please contact the administrator.';
-    // Submit a job to retrieve the histogram data
-    $.getJSON(url, function(data, status, jqXHR) {
-      if (status === 'success' && data['success'] === true) {
-        var payload = data['data'];
-        // Start polling the job status, displaying the histogram on success
-        poll(payload['job_id'], function(result) {
-          // We can only handle TH1F objects at the moment
-          if (result['data']['key_class'] !== 'TH1F') return;
-          displayHistogram(result['data']['key_data'], container);
-        }, function(result) {
-          displayFailure(container, failMsg);
-        });
-      } else {
+      var task = createTask('get_key_from_file', {filename: file, key_name: histogram});
+      task.done(function(job) {
+          displayHistogram(job['result']['data']['key_data'], container);
+      });
+      task.fail(function(message) {
+        var failMsg = '<p>There was a problem retrieving histogram '
+          + '<code>' + histogram + '</code>'
+          + ' from file '
+          + '<code>' + file + '</code>'
+          + '. Please contact the administrator.</p>'
+          + message;
         displayFailure(container, failMsg);
-      }
-    }).fail(function() { displayFailure(container, failMsg); });
+      });
   };
 
-  // Poll status of job, calling success or failure when finished.
-  // If the job is still running, `poll` is called again after `timeout`.
+  // Submit a job to the server
   // Accepts:
-  //   jobID: String ID of the job to poll
-  //   success: Function called on successful job completion, passed the response
-  //   failure: Function called on failed job completion, passed the response
-  //   timeout: Integer number of milliseconds to wait before calling poll again, if the job has not finished (default: WebMonitor.settings.pollRate)
-  var poll = function(jobID, success, failure, timeout) {
-    if (timeout === undefined) {
-      timeout = settings.pollRate;
+  //   taskName: Name of the task the job will run
+  //   args: Object of arguments passed to the task as named arguments
+  //   poll: Whether to poll the job until completion (default: true)
+  //     The job is polled in intervals of WebMonitor.settings.pollRate until
+  //     the job status is not `queued` or `started`.
+  // Returns:
+  //   jQuery.Deferred object which fires `resolve` on job completion and fires
+  //     `reject` on either submission failure or job polling failure
+  var submitJob = function(taskName, args, doPoll) {
+    if (doPoll === undefined) {
+      doPoll = true;
     }
-    setTimeout(function() {
-      $.getJSON('/fetch/' + jobID, function(data, stat, jqXHR) {
-        log('Polling jobID=' + jobID);
-        if (stat === 'success' && data['success'] === true) {
-          var payload = data['data'],
-              jobStatus = payload['status'],
-              result = payload['result'];
-          if (jobStatus === 'finished') {
-            log('Job ' + jobID + ' finished');
-            if (result['success'] === true) {
-              success(result);
-            } else {
-              failure(result);
-            }
-          } else if (jobStatus === 'failed') {
-            log('Job ' + jobID + ' failed');
-            failure(result);
-          } else {
-            log('Polling job ID ' + jobID + ': ' + jobStatus);
-            poll(jobID, success, failure, timeout);
-          }
+    var jobRequest =  $.ajax('/jobs', {
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      data: JSON.stringify({task_name: taskName, args: args})
+    });
+    var jobPromise = $.Deferred();
+    var poll = function(job) {
+      var jobID = job['id'],
+          jobStatus = job['status'];
+      // Poll the job if it hasn't not completed, else fire `resolve`
+      if (jobStatus === 'queued' || jobStatus === 'started') {
+        setTimeout(function() {
+          log('Polling job ID ' + jobID + ': ' + jobStatus);
+          $.getJSON(job['uri'])
+            .done(function(data, status) { poll(data['job']); })
+            .fail(function(data, status) { jobPromise.reject(data, status); });
+        }, settings.pollRate);
+      } else {
+        log('Job ' + jobID + ' completed: ' + jobStatus);
+        jobPromise.resolve(job);
+      }
+    };
+    jobRequest.done(function(data, status) {
+      var job = data['job'];
+      if (doPoll === true) {
+        poll(job);
+      }
+    });
+    // Fire `reject` if the submission fails
+    jobRequest.fail(function(data, status) { jobPromise.reject(data, status); });
+    return jobPromise;
+  };
+
+  // Create a task and submit a job for it to the queue
+  //
+  // Unlike submitJob, this method handles job failures by creating descriptive
+  // HTML error messages.
+  // This relies on the JSON response containing a `result` key which is the
+  // task's return value, which itself contains a `success` key determining
+  // whether the task completed successfully.
+  // If `success` if false, the task's `message` key is retrieved.
+  // Accepts:
+  //   taskName: Name of the task the job will run
+  //   args: Object of arguments passed to the task as named arguments
+  // Returns:
+  //   jQuery.Deferred object which fires `resolve` on job completion and fires
+  //     `reject` on either submission failure, job polling failure, or task
+  //     failure. On resolution, the JSON response object is passed, whereas
+  //     on rejection a descriptive HTML error message is passed.
+  var createTask = function(taskName, args) {
+    // Create a polling job
+    var jobPromise = submitJob(taskName, args, true);
+    var taskPromise = $.Deferred();
+    jobPromise.done(function(job) {
+      // Did the job complete successfully or not?
+      if (job['status'] === 'failed') {
+        var message = '<p>The job completed unsuccessfully.</p>';
+        taskPromise.reject(message);
+      } else {
+        // Did the task the job was running finish successfully or not?
+        var result = job['result'];
+        if (result['success'] === false) {
+          var message = '<p>The task could not complete successfully. It returned the error message '
+            + '<code>' + result['message'] + '</code>.'
+            + '</p>';
+          taskPromise.reject(message);
         } else {
-            failure(result);
+          taskPromise.resolve(job);
         }
-      });
-    }, timeout);
+      }
+    });
+    jobPromise.fail(function(data, status) {
+      var message;
+      try {
+        message = JSON.parse(data.responseText)['message'];
+      } catch(e) {
+        message = data.statusText;
+      }
+      message = '<p>The server responded with the error code '
+        + '<code>' + data.status + '</code>'
+        + ' and the message '
+        + '<code>' + message + '</code>.'
+        + '</p>';
+      taskPromise.reject(message);
+    });
+    return taskPromise;
+  };
+
+  // Display msg inside container, styled as a red error box
+  // Accepts:
+  //   container: jQuery element to insert msg into
+  //   msg: HTML message to display inside container
+  var displayFailure = function(container, msg) {
+    container.html('<div class="alert alert-danger">' + msg + '</div>');
   };
 
   // Add a `Spinner` object to the `element`, using `settings.spinnerDefaults` as options.
@@ -268,6 +322,8 @@ var WebMonitor = (function($, undefined) {
 
   return {
     init: init,
+    submitJob: submitJob,
+    createTask: createTask,
     settings: settings
   };
 })(jQuery);
