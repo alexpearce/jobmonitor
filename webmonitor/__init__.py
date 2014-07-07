1d525748a30753b28546699a90767850bf2c8950
Alex Pearce <alex@alexpearce.me>
2014-07-07 12:25:28 +0100

    Extract jobs API and catch-all router to Blueprints.
    App creation is now the instantiaion of a bare app, and then the addition of the jobs and catchall Blueprint objects.
    
    The examples/ templates have been extracted to a separate application, coming soon.

--- webmonitor/__init__.py
+++ webmonitor/__init__.py
@@ -3,8 +3,15 @@ from flask import (
     Flask
 )
 
-# Define the app and load its configuration from config.py
-app = Flask(__name__)
-app.config.from_object('webmonitor.config')
+def create_app():
+    # Define the app and load its configuration from config.py
+    app = Flask(__name__)
+    app.config.from_object('webmonitor.config')
 
-from webmonitor import views
+    # Add jobs API and generic views
+    from catchall import catchall
+    from jobs import jobs
+    app.register_blueprint(catchall)
+    app.register_blueprint(jobs)
+
+    return app
---
+++ webmonitor/jobs.py
@@ -0,0 +1,72 @@
+from flask import (
+    # Blueprint creation
+    Blueprint,
+    # The request context
+    request,
+    # JSON encode Python dictionaries
+    jsonify,
+    # Generate URLs
+    url_for,
+    # Raise HTTP error code exceptions
+    abort
+)
+
+# Job queues
+from rq import Queue
+from start_worker import conn
+queue = Queue(connection=conn)
+
+jobs = Blueprint('jobs', __name__)
+
+# TODO finalise response format, i.e. what metadata we send with each response
+def serialize_job(job):
+    """Return a dictionary representing the job."""
+    d = dict(
+        id=job.get_id(),
+        uri=url_for('jobs.get_job', job_id=job.get_id(), _external=True),
+        status=job.status,
+        result=job.result
+    )
+    return d
+
+
+@jobs.route('/jobs', methods=['GET'])
+def get_jobs():
+    jobs = [serialize_job(j) for j in queue.get_jobs()]
+    return jsonify({'jobs': jobs})
+
+
+@jobs.route('/jobs', methods=['POST'])
+def create_job():
+    data = request.get_json()
+    # TODO when Flask 0.11 is released, use request.is_json instead
+    if not data:
+        abort(400)
+    # Try read the task name and load the task, returning a 400 error on fail
+    try:
+        task_name = data['task_name']
+    except KeyError:
+        return jsonify(dict(
+            message='No task name provided'
+        )), 400
+    try:
+        # TODO we don't have introspection as we don't know what methods are
+        # to the worker. are there any sanity check we can do?
+        task = 'monitoring_app.tasks.{0}'.format(task_name)
+    except AttributeError:
+        return jsonify(dict(
+            message='Invalid task name `{0}`'.format(task_name)
+        )), 400
+    # Enqueue the task, passing empty arguments if none were provided
+    args = data.get('args', {})
+    job = queue.enqueue(task, **args)
+    return jsonify(dict(job=serialize_job(job))), 201
+
+
+@jobs.route('/jobs/<job_id>', methods=['GET'])
+def get_job(job_id):
+    # Try to fetch the job, 404'ing if it's not found
+    job = queue.safe_fetch_job(job_id)
+    if job is None:
+        abort(404)
+    return jsonify(dict(job=serialize_job(job)))
